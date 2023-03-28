8025da1d42dedcde3cb06c7f4078863e59686518
Tony.Wu <uq44961879@gmail.com>
2023-03-28 21:11:34 +1100

    <feat><product>: add features including creating, deleting, editing functions.

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,8 @@
-﻿using EMallMVC.Data;
+﻿using System;
+using EMallMVC.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using EMallMVC.Models;
 
 namespace EMallMVC.Controllers
 {
@@ -18,8 +20,142 @@ namespace EMallMVC.Controllers
             return _context.Product != null ?
                 View(await _context.Product.ToListAsync()) :
                 Problem("Entity set 'AppDbContext.Product'  is null.");
-            // return View();
         }
 
+        // GET: Product
+        public async Task<IActionResult> Details(int? productId)
+        {
+            Console.WriteLine("Route to GET Details ..................");
+            if (productId == null)
+            {
+                Console.WriteLine(productId);
+                Console.WriteLine("id {0} null ..................", productId);
+                return NotFound();
+            }
+
+            if (_context.Product == null)
+            {
+                Console.WriteLine("context product null ..................");
+                return NotFound();
+            }
+
+            var product = await _context.Product.FirstOrDefaultAsync(x => x.ProductId == productId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+
+        // GET: Product/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId, ProductName, ProductCategory, ProductDescription, ModifiedDate, Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync(); 
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(int? productId)
+        {
+            if (productId == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FindAsync(productId);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // POST: Products/Edit/5
+        public async Task<IActionResult> Edit(
+            int? productId, [Bind("ProductId, ProductName, ProductCategory, ProductDescription, ModifiedDate, Price")] Product product)
+        {
+            if (productId != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Product/Delete/5
+        public async Task<IActionResult> Delete(int? productId)
+        {
+            if (productId == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product.FirstOrDefaultAsync(m => m.ProductId == productId);
+            if (product == null )
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        //POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int productId)
+        {
+            if (_context.Product == null)
+            {
+                return Problem("Entity set 'AppDbContext.Product' is null.");
+            }
+            var product = await _context.Product.FindAsync(productId);
+            if (product != null)
+            {
+                _context.Product.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int productId)
+        {
+            return (_context.Product?.Any(e => e.ProductId == productId)).GetValueOrDefault();
+        }
     }
 }
